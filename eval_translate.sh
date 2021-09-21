413566b7ad168ec893b6b02e6e6fca1df056f69c
Uri Alon <ualon@tir.lti.cs.cmu.edu>
2021-09-21 13:00:37 -0400

    "uri"

--- eval_translate.sh
+++ eval_translate.sh
@@ -1,15 +1,17 @@
-val_source=
-val_target=
+val_source=../synpos/data/java-small-processed/val_src.txt
+val_target=../synpos/data/java-seq2seq-data/data.TargetType.seq.val.target.txt
 
-model_name=
+model_name=java_small_transformer_nofeat
 output_dir=${model_name}/validation_results/
+
+set -o xtrace
 
 mkdir -p ${output_dir}
 
 for model in $(ls -t ${model_name}/model_step_*.pt)
 do
-    output=${output_dir}/${model}.txt
-    python onmt/bin/translate.py -model ${model} -src ${val_target} -output ${output} -n_best 5 -beam_size 5 -gpu 0
+    output=${output_dir}/$(basename $model).txt
+    python onmt/bin/translate.py -model ${model} -src ${val_source} -output ${output} -n_best 5 -beam_size 5 -gpu 0
 done
 
 for translation in $(ls ${output_dir}/model*); do
