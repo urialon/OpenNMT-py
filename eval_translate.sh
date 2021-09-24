d8b6795b63bae3d623da6f2946158b6b0e6ee2a9
Uri Alon <ualon@tir-1-13.eth>
2021-09-24 10:34:57 -0400

    "uri"

--- eval_translate.sh
+++ eval_translate.sh
@@ -12,6 +12,7 @@ mkdir -p ${output_dir}
 
 for model in $(ls -t ${model_name}/model_step_*.pt)
 do
+    echo Translating ${model}...
     output=${output_dir}/$(basename $model).txt
     python onmt/bin/translate.py -model ${model} -src ${val_source} -output ${output} -n_best 5 -beam_size 5 -gpu 0
 done
@@ -26,13 +27,15 @@ for translation in $(ls ${output_dir}/model*); do
 best_model=$(basename $(cat ${output_dir}/validation_log.txt | grep 'Final' | sort -n -k 3 | tail -1 | cut -d' ' -f2))
 best_model=${best_model%.*}
 
+echo
 echo Best model: ${best_model}
+echo
 echo Best model validation scores:
 python eval_seq2seq.py --expected ${val_target} --actual ${output_dir}/${best_model}.txt
 
 echo
 echo Test results:
 test_output=${model_name}/test_translation_${best_model}
-python onmt/bin/translate.py -model ${best_model} -src ${test_source} -output ${test_output} -n_best 5 -beam_size 5 -gpu 0
+python onmt/bin/translate.py -model ${model_name}/${best_model} -src ${test_source} -output ${test_output} -n_best 5 -beam_size 5 -gpu 0
 python eval_seq2seq.py --expected ${test_target} --actual ${test_output}
 python eval_seq2seq.py --expected ${test_target} --actual ${test_output} > ${model_name}/results_${best_model}
