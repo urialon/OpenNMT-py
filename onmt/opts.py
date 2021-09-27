dde770046b43f7bc5bf227d01bd05dc89f9a9ea5
urialon <urialon1@gmail.com>
2021-09-27 15:49:04 -0400

    feat_merge: sharemlp, vocab size set to 2000

--- onmt/opts.py
+++ onmt/opts.py
@@ -237,9 +237,9 @@ def model_opts(parser):
 
     group = parser.add_argument_group('Model-Embedding Features')
     group.add('--feat_merge', '-feat_merge', type=str, default='concat',
-              choices=['concat', 'sum', 'mlp'],
+              choices=['concat', 'sum', 'mlp', 'sharemlp'],
               help="Merge action for incorporating features embeddings. "
-                   "Options [concat|sum|mlp].")
+                   "Options [concat|sum|mlp|sharemlp].")
     group.add('--feat_vec_size', '-feat_vec_size', type=int, default=-1,
               help="If specified, feature embedding sizes "
                    "will be set to this. Otherwise, feat_vec_exponent "
