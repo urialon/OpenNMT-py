5bc34b287f2996a32140da133c7b89bf26aaf90e
Uri Alon <ualon@tir.lti.cs.cmu.edu>
2021-10-14 22:02:54 -0400

    "uri"

--- run_train_transformer.sh
+++ run_train_transformer.sh
@@ -5,7 +5,8 @@ set -e
 model_name=$1
 
 mkdir ${model_name}
-python -u onmt/bin/train.py --config java_small_nofeat_share.yml --save_mode ${model_name}/model ${@:2}
+#python -u onmt/bin/train.py --config java_small_nofeat_share.yml --save_mode ${model_name}/model ${@:2}
+python -u onmt/bin/train.py --config java_med_nofeat.yml --save_mode ${model_name}/model ${@:2}
 
 notify-run send 'Finished training'
 
--- run_train_transformer_features.sh
+++ run_train_transformer_features.sh
@@ -5,7 +5,7 @@ set -e
 model_name=$1
 mkdir ${model_name}
 
-python -u onmt/bin/train.py --config java_small_features_shared_vocab.yaml --save_mode ${model_name}/model ${@:2}
+python -u onmt/bin/train.py --config java_med_nofeat.yml --save_mode ${model_name}/model ${@:2}
 
 notify-run send 'Finished training transformer'
 
