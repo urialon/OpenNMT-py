9ff22e91ffd5b42b022b32fb5522e301ae449fc0
urialon <urialon1@gmail.com>
2021-11-30 15:39:45 -0500

    analyze pred scripts

---
+++ scripts/analyze_pred.py
@@ -0,0 +1,131 @@
+import numpy as np
+from argparse import ArgumentParser
+import seaborn as sns
+import pandas as pd
+import matplotlib
+import matplotlib.pyplot as plt
+
+target_num_bins = 40
+target_min = 0
+target_max = 40
+
+src_num_bins = 25
+src_min = 0
+src_max = 2000
+
+def get_bin(value, num_bins, min, max):
+    bin = int(value // ((max - min) / num_bins))
+    if bin >= num_bins:
+        bin = num_bins - 1
+    return bin
+
+def binary_to_string(binary_string):
+    return binary_string.decode("utf-8")
+
+def binary_to_string_list(binary_string_list):
+    return [binary_to_string(w) for w in binary_string_list]
+
+def load_lines(filename):
+    with open(filename, 'rb') as file:
+        lines = [line.rstrip() for line in file.readlines()]
+        return binary_to_string_list(lines)
+
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument("--expected", required=True)
+    parser.add_argument("--src", required=True)
+    parser.add_argument("--actual_model", required=True)
+    parser.add_argument("--actual_baseline", required=True)
+    args = parser.parse_args()
+
+    expected_lines = load_lines(args.expected)
+    src_lines = load_lines(args.src)
+    actual_model_lines = load_lines(args.actual_model)
+    actual_baseline_lines = load_lines(args.actual_baseline)
+    beam_size = len(actual_model_lines) // len(expected_lines)
+
+    batched_model_hyps = [actual_model_lines[i*beam_size:(i+1)*beam_size] for i in range(len(expected_lines))]
+    batched_baseline_hyps = [actual_baseline_lines[i*beam_size:(i+1)*beam_size] for i in range(len(expected_lines))]
+
+    model_src_correct = np.zeros(src_num_bins)
+    model_src_count = np.zeros(src_num_bins)
+    model_target_correct = np.zeros(target_num_bins)
+    model_target_count = np.zeros(target_num_bins)
+
+    baseline_src_correct = np.zeros(src_num_bins)
+    baseline_src_count = np.zeros(src_num_bins)
+    baseline_target_correct = np.zeros(target_num_bins)
+    baseline_target_count = np.zeros(target_num_bins)
+
+    for src_line, expected, hyps in zip(src_lines, expected_lines, batched_model_hyps):
+        src_bin = get_bin(len(src_line.split()), src_num_bins, src_min, src_max)
+        target_bin = get_bin(len(hyps[0].split()), target_num_bins, target_min, target_max)
+        if expected == hyps[0]:
+            model_src_correct[src_bin] += 1
+            model_target_correct[target_bin] += 1
+        model_src_count[src_bin] += 1
+        model_target_count[target_bin] += 1                        
+
+    for src_line, expected, hyps in zip(src_lines, expected_lines, batched_baseline_hyps):
+        src_bin = get_bin(len(src_line.split()), src_num_bins, src_min, src_max)
+        target_bin = get_bin(len(hyps[0].split()), target_num_bins, target_min, target_max)
+        if expected == hyps[0]:
+            baseline_src_correct[src_bin] += 1
+            baseline_target_correct[target_bin] += 1
+        baseline_src_count[src_bin] += 1
+        baseline_target_count[target_bin] += 1
+
+    src_df = pd.DataFrame({'src_size': list(range(src_num_bins)) + list(range(src_num_bins)), 
+                            'acc': list(model_src_correct / model_src_count) + list(baseline_src_correct / baseline_src_count),
+                            'model': ['syntactic256'] * src_num_bins 
+                            + ['baseline'] * src_num_bins
+                            })
+    ax = sns.lineplot(data=src_df, x='src_size', y='acc', style='model', hue='model', markers=True)
+    xlabels = [int(x * (src_max - src_min) / src_num_bins) for x in ax.get_xticks()]
+    ax.set_xticklabels(xlabels)
+    matplotlib.pyplot.show()
+
+    min_src_df = pd.DataFrame({'min_src_size': list(range(src_num_bins)) + list(range(src_num_bins)), 
+                            'acc': list(model_src_correct[::-1].cumsum()[::-1] / model_src_count[::-1].cumsum()[::-1]) 
+                            + list(baseline_src_correct[::-1].cumsum()[::-1] / baseline_src_count[::-1].cumsum()[::-1]),
+                            'model': ['syntactic'] * src_num_bins 
+                            + ['baseline'] * src_num_bins
+                            })
+    ax = sns.lineplot(data=min_src_df, x='min_src_size', y='acc', style='model', hue='model', markers=True)
+    xlabels = [int(x * (src_max - src_min) / src_num_bins) for x in ax.get_xticks()]
+    ax.set_xticklabels(xlabels)
+    matplotlib.pyplot.show()
+
+    # for src_line, expected, hyps, baseline_hyps in zip(src_lines, expected_lines, batched_model_hyps, batched_baseline_hyps):
+    #     src_bin = get_bin(len(src_line.split()), src_num_bins, src_min, src_max)
+       
+    #     if expected == hyps[0] and expected != baseline_hyps[0]:
+    #         with open(f'logs/examples/bin{int(src_bin * (src_max - src_min) / src_num_bins)}.txt', 'a') as file:      
+    #             file.write(f'Context: \n{src_line}\n')
+    #             file.write(f'Ground truth: {expected.replace(" ", "")}\n')
+    #             file.write(f'Syntactic prediction: {hyps[0].replace(" ", "")}\n')
+    #             file.write(f'Baseline prediction: {baseline_hyps[0].replace(" ", "")}\n')
+    #             file.write('\n')
+
+    # tgt_df = pd.DataFrame({'tgt_size': list(range(target_num_bins)) + list(range(target_num_bins)), 
+    #                         'acc': list(model_target_correct / model_target_count) + list(baseline_target_correct / baseline_target_count),
+    #                         'model': ['syntactic'] * target_num_bins 
+    #                         + ['baseline'] * target_num_bins
+    #                         })
+    # ax = sns.lineplot(data=tgt_df, x='tgt_size', y='acc', style='model', hue='model', markers=True)
+    # xlabels = [int(x * (target_max - target_min) / target_num_bins) for x in ax.get_xticks()]
+    # ax.set_xticklabels(xlabels)
+    # matplotlib.pyplot.show()
+
+    # for bin in range(target_num_bins):
+    #     model_acc = model_target_correct[bin:].sum() / model_target_count[bin:].sum() * 100
+    #     baseline_acc = baseline_target_correct[bin:].sum() / baseline_target_count[bin:].sum() * 100
+    #     print(f'{int(bin * (target_max - target_min) / target_num_bins)}: Syntactic: {model_acc:.0f}, baseline: {baseline_acc:.0f}')
+
+    count_df = pd.DataFrame({'src_size': list(range(src_num_bins)), 
+                            'count': list(model_src_count),
+                            })
+    ax = sns.barplot(data=count_df, x='src_size', y='count')
+    xlabels = [int(x * (src_max - src_min) / src_num_bins) for x in ax.get_xticks()]
+    ax.set_xticklabels(xlabels)
+    matplotlib.pyplot.show()
--- scripts/create_eval_curve.py
+++ scripts/create_eval_curve.py
@@ -3,9 +3,36 @@ from argparse import ArgumentParser
 import pandas as pd
 import matplotlib
 
-max_ppl = 1000
 
 def load_training_log(filename):
+    steps = []
+    training_accuracies = []
+    validation_accuracies = []
+    last_training_step = None
+    last_training_acc = None
+    with open(filename, 'r') as f:
+        for line in f:
+            line = line.rstrip()
+            parts = line.split(' ')
+
+            if 'Validation' in parts:
+                if 'accuracy:' in parts:
+                    index_of_accuracy = parts.index('accuracy:')
+                    val_acc = float(parts[index_of_accuracy + 1])
+                    steps.append(last_training_step)
+                    training_accuracies.append(last_training_acc)
+                    validation_accuracies.append(val_acc)
+                continue
+            if 'Step' in parts:
+                index_of_step = parts.index('Step')
+                last_training_step = int(parts[index_of_step + 1].split('/')[0])
+                index_of_acc = parts.index('acc:')
+                last_training_acc = float(parts[index_of_acc + 2].rstrip(';'))
+            
+    return steps, training_accuracies, validation_accuracies
+
+
+def load_eval_log(filename):
     steps = []
     top1_accuracies = []
     top5_accuracies = []
@@ -25,14 +52,14 @@ def load_training_log(filename):
 
             if len(parts) > 0 and parts[0] == 'Final:':
                 step = int(parts[1].split('/')[-1].removeprefix('model_step_').removesuffix('.pt.txt'))
-                top1_acc = float(parts[2])
+                top1_acc = float(parts[2]) * 100
                 steps.append(step)
                 top1_accuracies.append(top1_acc)
                 top5_accuracies.append(last_top5)
                 continue
             if 'accuracy@1:' in parts and not is_beam_acc:
                 index_of_acc = parts.index('accuracy@1:') + 1
-                last_top5 = float(parts[index_of_acc])
+                last_top5 = float(parts[index_of_acc]) * 100
                 
             
     return steps, top1_accuracies, top5_accuracies
@@ -41,31 +68,53 @@ if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument("--log", required=True)
     parser.add_argument("--baseline_log", required=True)
+    parser.add_argument("--small_log", required=True)
+    parser.add_argument("--small_baseline_log", required=True)
+    # parser.add_argument("--train_log", required=True)
+    # parser.add_argument("--baseline_train_log", required=True)
     args = parser.parse_args()
 
-    steps, top1_accs, top5_accs = load_training_log(args.log)
-    baseline_steps, baseline_top1_accs, baseline_top5_accs = load_training_log(args.baseline_log)
+    steps, top1_accs, _ = load_eval_log(args.log)
+    small_steps, small_top1_accs, _ = load_eval_log(args.small_log)
+    baseline_steps, baseline_top1_accs, _ = load_eval_log(args.baseline_log)
+    small_nofeat_steps, small_nofeat_top1_accs, _ = load_eval_log(args.small_baseline_log)
+    # train_steps, training_accuracies, _ = load_training_log(args.train_log)
+    # baseline_train_steps, baseline_training_accuracies, _ = load_training_log(args.baseline_train_log)
     
-    # main_df = pd.DataFrame({'steps': steps + steps + baseline_steps + baseline_steps, 
-    #                         'acc': top1_accs + top5_accs + baseline_top1_accs + baseline_top5_accs, 
-    #                         'type': ['top1_acc'] * len(top1_accs) 
-    #                                 + ['top5_acc'] * len(top5_accs)
-    #                                 + ['top1_acc'] * len(baseline_top1_accs)
-    #                                 + ['top5_acc'] * len(baseline_top5_accs),
-    #                         'model': ['syntactic'] * len(steps) * 2
-    #                                 + ['baseline'] * len(baseline_steps) * 2})
-    main_df = pd.DataFrame({'steps': steps + baseline_steps, 
-                            'acc': top1_accs + baseline_top1_accs, 
+    
+    main_df = pd.DataFrame({'steps': steps + small_steps + baseline_steps + small_nofeat_steps, 
+                            'acc': top1_accs + small_top1_accs + baseline_top1_accs + small_nofeat_top1_accs, 
                             'type': ['top1_acc'] * len(top1_accs) 
-                                    + ['top1_acc'] * len(baseline_top1_accs),
-                            'model': ['syntactic'] * len(steps) * 1
-                                    + ['baseline'] * len(baseline_steps) * 1})
+                                    + ['top1_acc'] * len(small_top1_accs)
+                                    + ['top1_acc'] * len(baseline_top1_accs)
+                                    + ['top1_acc'] * len(small_nofeat_top1_accs),
+                            'model': ['syntactic'] * len(steps)
+                                    + ['syntactic256'] * len(small_steps)
+                                    + ['baseline'] * len(baseline_steps)
+                                    + ['baseline256'] * len(small_nofeat_steps)
+                                    })
+    # main_df = pd.DataFrame({'steps': steps + baseline_steps, 
+    #                         'acc': top1_accs + baseline_top1_accs,
+    #                         'model': ['syntactic'] * len(steps)
+    #                                 + ['baseline'] * len(baseline_steps)
+    #                                 })
+
 
     # ax = sns.scatterplot(data=main_df, x='steps', y='acc', style='type', hue='model', markers=True, s=5) # scatter_kws={'s':5}, line_kws={"color": "red"})
     # ax.yaxis.set_major_locator(matplotlib.ticker.MultipleLocator(50))
     # ax.set(ylim=(0, None))
-    matplotlib.pyplot.show()
     ax = sns.lineplot(data=main_df, x='steps', y='acc', style='type', hue='model', markers=True)
     xlabels = [f'{x//1000:.0f}K' for x in ax.get_xticks()]
     ax.set_xticklabels(xlabels)
     matplotlib.pyplot.show()
+
+    # main_df = pd.DataFrame({'steps': train_steps + baseline_train_steps, 
+    #                         'acc': training_accuracies + baseline_training_accuracies, 
+    #                         'type': ['train_acc'] * len(train_steps)
+    #                                 + ['train_acc'] * len(baseline_train_steps),
+    #                         'model': ['syntactic'] * len(train_steps)
+    #                                 + ['baseline'] * len(baseline_train_steps)})
+    # ax = sns.lineplot(data=main_df, x='steps', y='acc', style='type', hue='model', markers=True)
+    # xlabels = [f'{x//1000:.0f}K' for x in ax.get_xticks()]
+    # ax.set_xticklabels(xlabels)
+    # matplotlib.pyplot.show()
